2a3a2623854fbfb913478b647efbafa398ebfdef
Duri <durid@union.edu>
2014-07-07 06:14:30 -0400

    Add Enhancement Filters

---
+++ Thousand Words/VONFiltersViewController.h
@@ -0,0 +1,13 @@
+//
+//  VONFiltersViewController.h
+//  Thousand Words
+//
+//  Created by Duri Abdurahman Duri on 7/7/14.
+//  Copyright (c) 2014 von. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VONFiltersViewController : UICollectionViewController
+
+@end
